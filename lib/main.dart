1173fb5fe6120d007727cfcfeaafb4e0d195338e
priyanka <bharadwajpriyanka204@gmail.com>
2023-03-05 18:28:01 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:emi_calu/home.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
